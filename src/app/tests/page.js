6dbb971c1e35dc6385f40e8b801232bc0e010ae9
JeyM <jeymson8000@gmail.com>
2024-11-23 15:23:14 +0800

    before PE

--- src/app/tests/page.js
+++ src/app/tests/page.js
@@ -5,7 +5,7 @@ import Link from 'next/link';
 import TestForm from '@/components/TestForm';
 
 // FOR ADDING NEW TESTS
-export default function Employees() {
+export default function Tests() {
   // TO IGNORE NON-TEST TABLES
   const toFilter = [
     "appointment",
@@ -16,11 +16,16 @@ export default function Employees() {
     "person_address",
     "ref_job",
     "ref_test_type",
-    "staff"
+    "staff",
+    "junction_table"
   ]
 
   // SETS UP THE LIST OF TABLES AND MAKES THEM INTO OBJECTS
   const [tables, setTables] = useState({});
+  const [testTypes, setTestTypes] = useState([]);
+
+  const [chosenTest, setChosenTest] = useState({});
+
   const [error, setError] = useState("");
 
   // FOR THE POP-UP
@@ -35,7 +40,7 @@ export default function Employees() {
       }
       return res.json();
     } catch (err) {
-      console.error(`Fetch error for ${tableName}:`, err);
+      // console.error(`Fetch error for ${tableName}:`, err);
       setError(`Failed to fetch columns for ${tableName}`);
       return [];
     }
@@ -62,12 +67,56 @@ export default function Employees() {
     }
   };
 
+  async function fetchContent() {
+    const type = "query"
+    const destination = "ref_test_type";
+
+    const res = await fetch(
+      `/api/getData?type=${type}&table=${destination}`
+    );
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    const data = await res.json();
+    setTestTypes(data);
+  }
+
+  const handleEdit = async () => {
+    try {
+
+      console.log("chosenTest", chosenTest);
+
+      // const res = await fetch('/api/getData', {
+      //   method: 'PUT',
+      //   headers: { 'Content-Type': 'application/json' },
+      //   body: JSON.stringify({
+      //     tableName: "ref_test_type",
+      //     formData: chosenTest,
+      //     primaryKey: "test_name",
+      //     primaryValue: ""
+      //   })
+      // });
+
+      // const response = await res.json();
+
+      // if (!res.ok) {
+      //   // setMessage(response.error);
+      // } else {
+      //   // setMessage(response.message);
+      //   // onClose();
+      // }
+    } catch (error) {
+      console.log(`Error: ${error.message}`);
+    }
+  };
+
+
   // TRIGGERS THE TABLE CHECK UPON START AND OPENING AND CLOSING THE POP-UP
   useEffect(() => {
     fetchTables();
-  }, [adding]);
+    fetchContent();
 
-  console.log(tables);
+  }, [adding, chosenTest]);
 
   return (
     <div className='view-appointments-page background'>
@@ -87,19 +136,33 @@ export default function Employees() {
         </div>
 
         <div className="selection-grid">
-          {Object.entries(tables).map(([tableName, columns], index) => (
-            <div key={index} className="test-item">
-                <div className="set-name text-medium-dark-bold">{tableName} <span className='text-medium-dark' style={{marginLeft:"auto"}}>Cost: </span> 
-                <input className='form-input text-small-dark' style={{width:"25%", marginRight:"0"}}></input>
-                <button className='dark-button text-small-light' style={{width:"min-content"}} >Modify</button></div>
+          {Object.entries(tables).map(([tableName, columns], index) => {
+            const matchingTest = testTypes.find(test => {
+              return test.test_name == tableName.toUpperCase()
+            });
 
-                <div className='test-fields'>
+            return <div key={index} className="test-item">
+              <div className="set-name text-medium-dark-bold">{matchingTest.test_name} <span className='text-medium-dark' style={{ marginLeft: "auto" }}>Cost: </span>
+                <input className='form-input text-small-dark' value={matchingTest.test_price} onChange={(e) => {
+                  setTestTypes(prevTestTypes =>
+                    prevTestTypes.map(test =>
+                      test.test_name === matchingTest.test_name
+                        ? { ...test, test_price: e.target.value }
+                        : test
+                    )
+                  );
+                }}
+
+                  style={{ width: "25%", marginRight: "0" }}></input>
+              </div>
+
+              <div className='test-fields'>
                 {columns.map((column, idx) => (
                   <h3 key={idx} className="text-small-white"><span className="text-small-white-bold">{column.COLUMN_NAME}:</span> ({column.COLUMN_TYPE})</h3>
                 ))}
-                </div>
-        </div>
-          ))}
+              </div>
+            </div>
+          })}
         </div>
 
 
