554fd8999c70a21d4978657bbb06bf87251c679d
Viennbalcita <vienn_rowen_balcita@dlsu.edu.ph>
2024-11-23 13:47:06 +0800

    Added address fields to the employeeForm

--- src/components/EmployeeForm.js
+++ src/components/EmployeeForm.js
@@ -64,26 +64,10 @@ const EmployeeForm = ({ selectedEmployee, setSelectedEmployee, onClose, handleSa
             phone_number: formData.phone,
             email: formData.email,
             sex: formData.sex,
+            street_name: formData.street,
+            city_name: formData.city,
+            province_name: formData.province,
         };
-        try {
-            //Submit data to the database
-          const res = await fetch('/api/getData?type=', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ tableName: destination, formData: inputData })
-          });
-          //checks if submit was successful
-          const response = await res.json();
-          if (res.ok) {
-            setSuccessMessage2(response.message);
-            onClose();
-          } else {
-            setErrorState(response.error);
-          }
-        } catch (err) {
-          setErrorState('Failed to insert data');
-        }
-
         try {
             //Submit data to the database
           const res = await fetch('/api/getData?type=add', {
@@ -102,6 +86,26 @@ const EmployeeForm = ({ selectedEmployee, setSelectedEmployee, onClose, handleSa
         } catch (err) {
           setErrorState('Failed to insert data');
         }
+        /*
+        try {
+            //Submit data to the database
+          const res = await fetch('/api/getData?type=add', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ tableName: destination, formData: inputData })
+          });
+          //checks if submit was successful
+          const response = await res.json();
+          if (res.ok) {
+            setSuccessMessage2(response.message);
+            onClose();
+          } else {
+            setErrorState(response.error);
+          }
+        } catch (err) {
+          setErrorState('Failed to insert data');
+        }
+        
         // Filling out the ref_job table
         const inputData2 = {
             job_name: formData.job,
@@ -124,7 +128,7 @@ const EmployeeForm = ({ selectedEmployee, setSelectedEmployee, onClose, handleSa
         } catch (err) {
           setErrorState('Failed to insert data');
         }
-
+        */
         //TODO: Have to query to put in staff table
     };
     // SENDS FORM DATA TO THE ORIGINAL PAGE
